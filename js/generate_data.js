f3fc656d1475fe7cbca756b1a182bc44a7de81b3
BaoDNguyen <Bao.D.Nguyen@ttu.edu>
2020-03-28 00:34:38 -0500

    add more curves

--- js/generate_data.js
+++ js/generate_data.js
@@ -28,4 +28,38 @@ class Generate_data {
     return result;
   }
 
+  // generate curve of the following equations:
+  // x = A0*cos(t-t0)+t
+  // y = A1*cos(t-t1)+t
+  // t: variable
+  // A0, A1, t0, t1: constants
+  // tRange: [beginning value, ending value]
+  HarmonicFunction(A0,A1,t0,t1,tRange,numberOfPoint) {
+    let result = [];
+    let size = (tRange[1]-tRange[0])/(numberOfPoint-1);
+    for (let i = 0; i < numberOfPoint; i++) {
+      let t = tRange[0]+i*size;
+      let x = A0*Math.cos(t-t0)+t;
+      let y = A1*Math.cos(t-t1)+t;
+      result[i] = [x,y];
+    }
+    return result;
+  }
+
+  // generate ellipse
+  // equation in polar coordinates:
+  // r = ab/sqrt(a*a*sin(theta-theta0)*sin(theta-theta0)+b*b*cos(theta-theta0)*cos(theta-theta0))
+  uniformEllipse(a,b,theta0,numberOfPoint) {
+    let result = [];
+    let size = Math.PI*2/(numberOfPoint-1);
+    for (let i = 0; i < numberOfPoint; i++) {
+      let theta = i*size;
+      let r = a*b/Math.sqrt(a*a*Math.sin(theta-theta0)*Math.sin(theta-theta0)+b*b*Math.cos(theta-theta0)*Math.cos(theta-theta0));
+      let x = r*Math.cos(theta);
+      let y = r*Math.sin(theta);
+      result[i] = [x,y];
+    }
+    return result;
+  }
+
 }
